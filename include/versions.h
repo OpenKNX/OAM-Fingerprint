fd2999fd363793e50e43fd99f29ef89d4a178a10
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2023-12-01 19:46:23 +0100

    Updated to latest OpenKNX dependency versions.

---
+++ include/versions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define MAIN_Version "45132c4"
+#define KNX_Version "e602b59"
+#define MODULE_Common_Version "1.0.9+b4610cb"
+#define MODULE_Common_ETS 16
+#define MODULE_LogicModule_Version "2.0.3+6212270"
+#define MODULE_LogicModule_ETS 32
+#define MODULE_VirtualButton_Version "0.3.0+80d72af"
+#define MODULE_VirtualButton_ETS 3
+#define MODULE_FileTransferModule_Version "0.0.4+bccd3ad"
+#define MODULE_FileTransferModule_ETS 0
