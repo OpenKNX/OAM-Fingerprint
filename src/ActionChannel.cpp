13b9d938c99a08e01f37e26f94dfffa2b099f903
Waldemar Porscha <wp@porscha.eu>
2024-05-14 01:22:06 +0200

    FIX: Action execution
    - actionId was missing a -1
    - Actions were sent to the state ko instead of switch ko
    - fixed wrong index for dpt 15

--- src/ActionChannel.cpp
+++ src/ActionChannel.cpp
@@ -45,13 +45,13 @@ void ActionChannel::processScan(uint16_t location)
             case 0: // action deactivated
                 break;
             case 1: // switch
-                KoFIN_ActionState.value(ParamFIN_ActionOnOff, DPT_Switch);
+                KoFIN_ActionSwitch.value(ParamFIN_ActionOnOff, DPT_Switch);
                 break;
             case 2: // toggle
-                KoFIN_ActionState.value(!KoFIN_ActionState.value(DPT_Switch), DPT_Switch);
+                KoFIN_ActionSwitch.value(!KoFIN_ActionState.value(DPT_Switch), DPT_Switch);
                 break;
             case 3: // stair light
-                KoFIN_ActionState.value(true, DPT_Switch);
+                KoFIN_ActionSwitch.value(true, DPT_Switch);
                 _stairLightTime = millis();
                 break;
         }
