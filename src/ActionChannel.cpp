e26521084c5f95ad1d95b7576c82c32fbbc35754
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2024-05-13 16:43:11 +0200

    Implemented basic action processing.

--- src/ActionChannel.cpp
+++ src/ActionChannel.cpp
@@ -15,18 +15,35 @@ void ActionChannel::processInputKo(GroupObject &ko)
     switch (FIN_KoCalcIndex(ko.asap()))
     {
         case FIN_KoActionCall:
-            // start timer
-            // reset KoFIN_ActionCall after timer
+            // #TODO start timer
+            // #TODO reset KoFIN_ActionCall after timer
+            KoFIN_ActionCall.value(true, DPT_Switch);
             break;
     }
-
-
 }
 
 void ActionChannel::processScan(uint16_t location)
 {
     if (!ParamFIN_ActionAuthenticate || KoFIN_ActionCall.value(DPT_Switch))
     {
+        switch (ParamFIN_ActionActionType)
+        {
+            case 0: // action deactivated
+                break;
+            case 1: // switch
+                KoFIN_ActionSwitch.value(ParamFIN_ActionOnOff, DPT_Switch);
+                KoFIN_ActionState.value(ParamFIN_ActionOnOff, DPT_Switch);
+                break;
+            case 2: // toggle
+                KoFIN_ActionSwitch.value(!KoFIN_ActionState.value(DPT_Switch), DPT_Switch);
+                KoFIN_ActionState.value(!KoFIN_ActionState.value(DPT_Switch), DPT_Switch);
+                break;
+            case 3: // stair light
+                // #TODO
+                break;
+        }
 
+        if (KoFIN_ActionCall.value(DPT_Switch))
+            KoFIN_ActionCall.value(false, DPT_Switch);
     }
 }
